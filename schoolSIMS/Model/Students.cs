737f79b42bce06c3a3bc184f6ef6e0d4f056e2eb
Kelvin <39424472+Frosty273@users.noreply.github.com>
2019-04-30 13:54:53 +1200

    Add files via upload

---
+++ schoolSIMS/Model/Students.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace schoolSIMS.Model
+{
+    [Table("students")]
+    public partial class Students
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("first_name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Column("phone_name")]
+        [StringLength(20)]
+        public string PhoneName { get; set; }
+        [Column("email")]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Column("house_address")]
+        [StringLength(50)]
+        public string HouseAddress { get; set; }
+        [Column("gender")]
+        [StringLength(5)]
+        public string Gender { get; set; }
+        [Column("dob", TypeName = "date")]
+        public DateTime? Dob { get; set; }
+        [Column("date_created", TypeName = "date")]
+        public DateTime? DateCreated { get; set; }
+    }
+}
